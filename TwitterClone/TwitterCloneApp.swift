433b6facf6e7a95283849f00fda39489946ec721
Rafael <rafael.ahmedov@gmail.com>
2021-07-11 19:28:48 +0400

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Rafael.Ahmedov on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
